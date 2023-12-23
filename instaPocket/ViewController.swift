0d269ebd703cb0af487ef5f63e7d4dfdc98fc43d
rwrun <islamrun@gmail.com>
2023-12-23 19:04:48 +0500

    Initial Commit

---
+++ instaPocket/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  instaPocket
+//
+//  Created by Ислам Батыргереев on 23.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
