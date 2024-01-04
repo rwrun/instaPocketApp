192f046935f318302d4cc766ac6d40997267382f
rwrun <islamrun@gmail.com>
2024-01-04 20:04:07 +0500

    Рабочая шапка приложения. Готова верстка ячейки главной коллекции

--- instaPocket/Module/MainScreenModule/View/MainScreenView.swift
+++ instaPocket/Module/MainScreenModule/View/MainScreenView.swift
@@ -14,6 +14,32 @@ protocol MainScreenViewProtocol: AnyObject{
 class MainScreenView: UIViewController {
 
     var presenter: MainScreenPresenterProtocol!
+    private var topInsets: CGFloat = 0
+    private var menuViewHeight = UIApplication.topSafeArea + 70
+    
+    private lazy var topMenuView: UIView = {
+        $0.frame = CGRect(x: 0, y: 0, width: view.bounds.width, height: menuViewHeight)
+        $0.backgroundColor = .appMain
+        $0.addSubview(menuAppName)
+        $0.addSubview(settingButton)
+        return $0
+    }(UIView())
+    
+    
+    private lazy var menuAppName: UILabel = {
+        $0.text = "instaPocket"
+        $0.font = UIFont.systemFont(ofSize: 30, weight: .bold)
+        $0.textColor = .white
+        $0.frame = CGRect(x: 50, y: menuViewHeight - 40, width: view.bounds.width, height: 30)
+        return $0
+    }(UILabel())
+    
+    private lazy var settingButton: UIButton = {
+        $0.frame = CGRect(x: view.bounds.width - 50, y: menuViewHeight - 35, width: 20, height: 20)
+        $0.setBackgroundImage(UIImage(systemName: "gearshape"), for: .normal)
+        $0.tintColor = .white
+        return $0
+    }(UIButton())
     
     private lazy var collectionView: UICollectionView = {
         
@@ -24,6 +50,7 @@ class MainScreenView: UIViewController {
         layout.minimumLineSpacing = 30
         layout.sectionInset = UIEdgeInsets(top: 15, left: 0, bottom: 40, right: 0)
         
+        $0.contentInset.top = 130
         $0.backgroundColor = .appMain
         $0.dataSource = self
         $0.delegate = self
@@ -40,6 +67,9 @@ class MainScreenView: UIViewController {
 
         view.backgroundColor = .appMain
         view.addSubview(collectionView)
+        view.addSubview(topMenuView)
+        
+        topInsets = collectionView.adjustedContentInset.top
     }
     
 
@@ -60,6 +90,11 @@ extension MainScreenView: UICollectionViewDataSource, UICollectionViewDelegate, 
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: MainPostCell.reuseId, for: indexPath) as? MainPostCell
         else { return UICollectionViewCell() }
         
+        if let item = presenter.posts?[indexPath.section].items[indexPath.row] {
+            cell.configureCell(item: item)
+        }
+        
+        
         cell.backgroundColor = .gray
         return cell
     }
@@ -76,6 +111,16 @@ extension MainScreenView: UICollectionViewDataSource, UICollectionViewDelegate, 
         CGSize(width: view.frame.width - 60, height: 40)
     }
     
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let menuTopPosition = scrollView.contentOffset.y + topInsets
+        
+        if menuTopPosition < 40, menuTopPosition > 0{
+            topMenuView.frame.origin.y = -menuTopPosition
+            self.menuAppName.font = UIFont.systemFont(ofSize: 30 - menuTopPosition * 0.2,
+                                                      weight: .bold)
+        }
+    }
     
 }
 
